579c95d3a017cb7455baec06c023141e6e3d5fac
Rick Patci <rpatci@gmail.com>
2016-06-15 08:05:00 -0700

    push starter-code class-08

--- starter-code/scripts/models/article.js
+++ starter-code/scripts/models/article.js
@@ -8,44 +8,41 @@
     },this); // Specify that 'this' is to be bound to the constructor instance.
   }
 
-  Article.all = [];
+  Article.allArticles = [];
 
   Article.prototype.toHtml = function(scriptTemplateId) {
     var template = Handlebars.compile(scriptTemplateId.text());
-
     this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
     this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
     this.body = marked(this.body);
-
     return template(this);
   };
 
-  // TODO: Set up a DB table for articles.
-  Article.createTable = function(callback) {
+  // Set up a DB table for articles.
+  Article.createTable = function() {
     webDB.execute(
-      '...;', // what SQL command do we run here inside these quotes?
-      function(result) {
-        console.log('Successfully set up the articles table.', result);
-        if (callback) callback();
+      '...;', // TODO: What SQL command do we run here inside these quotes?
+      function() {
+        console.log('Successfully set up the articles table.');
       }
     );
   };
 
   // DONE: Refactor to expect the data from the database, rather than localStorage.
   Article.loadAll = function(rows) {
-    Article.all = rows.map(function(ele) {
+    Article.allArticles = rows.map(function(ele) {
       return new Article(ele);
     });
   };
 
-  Article.fetchAll = function(next) {
-    /* TODO: Refactor the .fetchAll() method to check
+  Article.fetchAll = function(nextFunction) {
+    /* TODO: Refactor this method to check
         if the database holds any records or not.
 
       If the DB has data already, we'll load up the data
         (most recent article first!), and then hand off control to the View.
+      Otherwise (if the DB is empty) we need to retrieve the JSON and process it. */
 
-      If the DB is empty, we need to retrieve the JSON and process it. */
     webDB.execute('...', function(rows) { // TODO: fill these quotes to 'select' our table.
       if (rows.length) {
         /* TODO:
@@ -54,19 +51,19 @@
                 was passed in to Article.fetchAll */
 
       } else {
-        $.getJSON('/data/hackerIpsum.json', function(data) {
+        $.getJSON('/data/hackerIpsum.json', function(responseData) {
           // Save each article from this JSON file, so we don't need to request it next time:
-          data.forEach(function(obj) {
+          responseData.forEach(function(obj) {
             var article = new Article(obj); // This will instantiate an article instance based on each article object from our JSON.
             /* TODO:
                1 - 'insert' the newly-instantiated article in the DB:
-                (hint: what can we call on each 'article' instance?). */
+                (hint: what can we call on this article instance?). */
 
           });
           // Now get ALL the records out the DB, with their database IDs:
           webDB.execute('', function(rows) { // TODO: select our now full table
             // TODO:
-            // 1 - Use Article.loadAll to instanitate these rows,
+            // 1 - Use Article.loadAll to generate our rows,
             // 2 - Pass control to the view by calling the next function that was passed in to Article.fetchAll
 
           });
@@ -75,74 +72,70 @@
     });
   };
 
-  Article.prototype.insertRecord = function(callback) {
+  Article.prototype.insertRecord = function() {
     webDB.execute(
       [
         {
           // TODO: Insert an article instance into the database:
-          // Note: this method will be called elsewhere after we retrieve our JSON
-          'sql': '...;',
+          // NOTE: this method will be called elsewhere after we retrieve our JSON
+          'sql': '...;', // <----- complete our SQL command here, inside the quotes.
           'data': [this.title, this.author, this.authorUrl, this.category, this.publishedOn, this.body]
-        } 
-      ],
-      callback
+        }
+      ]
     );
   };
 
-  Article.prototype.updateRecord = function(callback) {
+  Article.prototype.updateRecord = function() {
     webDB.execute(
       [
         {
           /* TODO: Update an article instance, overwriting
               its properties into the corresponding record in the database: */
           /* Note: this is an advanced admin option, so you will need to test
-              out an individual query in the SQL console */
-          'sql': '...;',
+              out an individual query in the console */
+          'sql': '...;', // <---- complete the command here, inside the quotes;
           'data': [this.title, this.author, this.authorUrl, this.category, this.publishedOn, this.body, this.id]
         }
-      ],
-      callback
+      ]
     );
   };
 
-  Article.prototype.deleteRecord = function(callback) {
+  Article.prototype.deleteRecord = function() {
     webDB.execute(
       [
         {
           // TODO: Delete an article instance from the database based on its id:
           /* Note: this is an advanced admin option, so you will need to test
-              out an individual query in the SQL console */
-          'sql': '...;',
+              out an individual query in the console */
+          'sql': '...;', // <--- complete the command here, inside the quotes;
           'data': [this.id]
         }
-      ],
-      callback
+      ]
     );
   };
 
-  Article.truncateTable = function(callback) {
+  Article.truncateTable = function() {
     webDB.execute(
       // TODO: Use correct SQL syntax to delete all records from the articles table.
-      'DELETE ...;', // <----finish the command here, inside the quotes.
-      callback
+      'DELETE ...;' // <----finish the command here, inside the quotes.
     );
   };
 
   Article.allAuthors = function() {
-    return Article.all.map(function(article) {
+    return Article.allArticles.map(function(article) {
       return article.author;
     })
-    .reduce(function(names, name) {
-      if (names.indexOf(name) === -1) {
-        names.push(name);
+    .reduce(function(uniqueNames, name) {
+      if (uniqueNames.indexOf(name) === -1) {
+        uniqueNames.push(name);
       }
-      return names;
+      return uniqueNames;
     }, []);
   };
 
   Article.numWordsAll = function() {
-    return Article.all.map(function(article) {
-      return article.body.match(/\b\w+/g).length;
+    return Article.allArticles.map(function(article) {
+      return article.body.match(/\w+/g).length;
     })
     .reduce(function(a, b) {
       return a + b;
@@ -150,17 +143,17 @@
   };
 
   Article.numWordsByAuthor = function() {
-    return Article.allAuthors().map(function(author) {
+    return Article.allAuthors().map(function(currentAuthor) {
       return {
-        name: author,
-        numWords: Article.all.filter(function(a) {
-          return a.author === author;
+        name: currentAuthor,
+        numWords: Article.allArticles.filter(function(article) {
+          return article.author === currentAuthor;
         })
-        .map(function(a) {
-          return a.body.match(/\b\w+/g).length;
+        .map(function(currentAuthorsArticle) {
+          return currentAuthorsArticle.body.match(/\w+/g).length;
         })
-        .reduce(function(a, b) {
-          return a + b;
+        .reduce(function(previousWords, currentWords) {
+          return previousWords + currentWords;
         })
       };
     });
@@ -168,11 +161,12 @@
 
   Article.stats = function() {
     return {
-      numArticles: Article.all.length,
+      numArticles: Article.allArticles.length,
       numWords: Article.numwords(),
       Authors: Article.allAuthors(),
     };
   };
+// TODO: ensure that our table has been setup.
 
   module.Article = Article;
 })(window);
