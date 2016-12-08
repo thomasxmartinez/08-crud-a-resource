68e5c00812d85d6853c5a7e1518b72212b9e3381
mikemiksch <mikemiksch@gmail.com>
2016-12-07 17:01:33 -0800

    Everything's finished, huzzah!

--- starter-code/scripts/models/article.js
+++ starter-code/scripts/models/article.js
@@ -20,7 +20,7 @@
   Article.createTable = function() {
     // webDb helps us query our data
     webDB.execute(
-      'CREATE TABLE IF NOT EXISTS articles (id INTEGER PRIMARY KEY, title VARCHAR, category VARCHAR, author VARCHAR, authorUrl VARCHAR, publishedOn VARCHAR, body VARCHAR);', // TODO: What SQL command do we run here inside these quotes?
+      'CREATE TABLE IF NOT EXISTS articles (id INTEGER PRIMARY KEY, title VARCHAR, category VARCHAR, author VARCHAR, authorUrl VARCHAR, publishedOn VARCHAR, body VARCHAR);', // TODONE: What SQL command do we run here inside these quotes?
       function() {
         console.log('Successfully set up the articles table.');
       }
@@ -31,6 +31,7 @@
   Article.loadAll = function(rows) {
     Article.allArticles = rows.map(function(ele) {
       return new Article(ele);
+      console.log('Article.loadAll has fired');
     });
   };
 
@@ -38,7 +39,7 @@
     webDB.execute(
       [{
         // NOTE: this method will be called elsewhere after we retrieve our JSON
-        'sql': 'INSERT INTO articles (title, category, author, authorUrl, publishedOn, body) VALUES(?, ?, ?, ?, ?, ?);', // <----- TODO: complete our SQL query here, inside the quotes.
+        'sql': 'INSERT INTO articles (title, category, author, authorUrl, publishedOn, body) VALUES(?, ?, ?, ?, ?, ?);', // <----- TODONE: complete our SQL query here, inside the quotes.
         'data': [this.title, this.category, this.author, this.authorUrl, this.publishedOn, this.body]
       }]
     );
@@ -46,15 +47,15 @@
 
   Article.fetchAll = function(nextFunction) {
     webDB.execute(
-      'SELECT * FROM articles;', // <-----TODO: fill these quotes to query our table.
+      'SELECT * FROM articles;', // <-----TODONE: fill these quotes to query our table.
       function(rows) {
         // if we have data in the table
         if (rows.length) {
-          /* TODO:
+          /* TODONE:
           1 - Use Article.loadAll to instanitate these rows,
           2 - invoke the function that was passed in to fectchAll */
           Article.loadAll(rows);
-          nextFunction;
+          nextFunction();
         } else {
           $.getJSON('/data/hackerIpsum.json', function(responseData) {
             responseData.forEach(function(obj) {
@@ -65,78 +66,78 @@
               article.insertRecord();
             });
             webDB.execute(
-              'SELECT * FROM articles;', // <-----TODO: query our table for articles once more
+              'SELECT * FROM articles;', // <-----TODONE: query our table for articles once more
               function(rows) {
-                // TODO:
+                // TODONE:
                 // 1 - Use Article.loadAll to process our rows,
                 // 2 - invoke the function that was passed in to fetchAll
                 Article.loadAll(rows);
-                nextFunction;
+                nextFunction();
               });
-            });
-          }
-        });
-      };
+          });
+        }
+      });
+  };
 
 
-      Article.prototype.deleteRecord = function() {
-        webDB.execute(
-          [
-            {
-              /* NOTE: this is an advanced admin option, so you will need to test
-              out an individual query in the console */
-              'sql': '', // <---TODO: Delete an article instance from the database based on its id:
-              'data': [this.id]
-            }
-          ]
-        );
-      };
+  Article.prototype.deleteRecord = function() {
+    webDB.execute(
+      [
+        {
+          /* NOTE: this is an advanced admin option, so you will need to test
+          out an individual query in the console */
+          'sql': 'DELETE FROM articles WHERE id = ?', // <---TODONE: Delete an article instance from the database based on its id:
+          'data': [this.id]
+        }
+      ]
+    );
+  };
 
-      Article.clearTable = function() {
-        webDB.execute(
-          'DELETE ...;' // <----TODO: delete all records from the articles table.
-        );
-      };
+  Article.clearTable = function() {
+    webDB.execute(
+      'DELETE FROM articles;' // <----TODONE: delete all records from the articles table.
+    );
+  };
 
-      Article.allAuthors = function() {
-        return Article.allArticles.map(function(article) {
-          return article.author;
+  Article.allAuthors = function() {
+    return Article.allArticles.map(function(article) {
+      return article.author;
+    })
+    .reduce(function(uniqueNames, curName) {
+      if (uniqueNames.indexOf(curName) === -1) {
+        uniqueNames.push(curName);
+      }
+      return uniqueNames;
+    }, []);
+  };
+
+  Article.numWordsAll = function() {
+    return Article.allArticles.map(function(article) {
+      return article.body.match(/\w+/g).length;
+    })
+    .reduce(function(a, b) {
+      return a + b;
+    });
+  };
+
+  Article.numWordsByAuthor = function() {
+    return Article.allAuthors().map(function(currentAuthor) {
+      return {
+        name: currentAuthor,
+        numWords: Article.allArticles.filter(function(article) {
+          return article.author === currentAuthor;
         })
-        .reduce(function(uniqueNames, curName) {
-          if (uniqueNames.indexOf(curName) === -1) {
-            uniqueNames.push(curName);
-          }
-          return uniqueNames;
-        }, []);
-      };
-
-      Article.numWordsAll = function() {
-        return Article.allArticles.map(function(article) {
-          return article.body.match(/\w+/g).length;
+        .map(function(currentAuthorsArticle) {
+          return currentAuthorsArticle.body.match(/\w+/g).length;
         })
-        .reduce(function(a, b) {
-          return a + b;
-        });
+        .reduce(function(previousWords, currentWords) {
+          return previousWords + currentWords;
+        })
       };
-
-      Article.numWordsByAuthor = function() {
-        return Article.allAuthors().map(function(currentAuthor) {
-          return {
-            name: currentAuthor,
-            numWords: Article.allArticles.filter(function(article) {
-              return article.author === currentAuthor;
-            })
-            .map(function(currentAuthorsArticle) {
-              return currentAuthorsArticle.body.match(/\w+/g).length;
-            })
-            .reduce(function(previousWords, currentWords) {
-              return previousWords + currentWords;
-            })
-          };
-        });
-      };
+    });
+  };
 
       // TODO: ensure that our table has been created.
 
-      module.Article = Article;
-    })(window);
+  module.Article = Article;
+})(window);
